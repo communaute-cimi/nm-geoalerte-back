2d6be6c7398f8b82f896131cb6be771d432ed250
Christophe Bougère <christophe.bougere@gmail.com>
2016-01-16 18:56:22 +0100

    amélioration de l'API (non prise en compte a udernier commit

--- src/routes.php
+++ src/routes.php
@@ -5,25 +5,63 @@
  * Get all alerts
  */
 $app->get('/alerts', function ($request, $response, $args) {
-    $statement = $this->database
+    $select = $this->database
       ->select()
       ->from('alert')
       ->execute()
     ;
 
-    $data = $statement->fetch();
+    $alerts = $select->fetch();
 
     return $response
       ->withHeader('Content-type', 'application/json')
       ->withStatus(200)
-      ->write(json_encode($data))
+      ->write(json_encode($alerts))
     ;
+});
+
+
+/**
+ * Create an alert
+ */
+$app->post('/alerts', function ($request, $response, $args) {
+  $message = $args['message'];
+  $longMessage = $args['long_message'];
+  $category = $args['category'];
+  $url = $args['url'];
+  $geom = $args['geom'];
+
+  $id = $this->database
+    ->insert(array(
+      'message',
+      'short_message',
+      'long_message',
+      'short_message',
+      'category',
+      'url',
+      'geom'
+    ))
+    ->into('alert')
+    ->values(
+      $message,
+      $longMessage,
+      $category,
+      $url,
+      $geom
+    )
+  ->execute();
+
+  return $response
+    ->withHeader('Content-type', 'application/json')
+    ->withStatus(200)
+    ->write(json_encode($id))
+  ;
 });
 
 /**
  * Retrieve alerts from a point
  */
-$app->get('/alerts/findByPoint', function ($request, $response, $args) {
+/*$app->get('/alerts/findByPoint', function ($request, $response, $args) {
     $lat = $args['lat'];
     $lon = $args['lon'];
 
@@ -41,12 +79,12 @@ $app->get('/alerts/findByPoint', function ($request, $response, $args) {
       ->withStatus(200)
       ->write(json_encode($data))
     ;
-});
+});*/
 
 /**
  * Retrieve alerts from a path
  */
-$app->get('/alerts/findByPath', function ($request, $response, $args) {
+/*$app->get('/alerts/findByPath', function ($request, $response, $args) {
     $start_lat = $args['start_lat'];
     $start_lon = $args['start_lon'];
     $dest_lat = $args['dest_lat'];
@@ -66,4 +104,4 @@ $app->get('/alerts/findByPath', function ($request, $response, $args) {
       ->withStatus(200)
       ->write(json_encode($data))
     ;
-});
+});*/
